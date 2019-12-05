765a1d465440413c342ed66109a6dc13d1d98203
Eduardo Bonilla <ebonilla88@gmail.com>
2019-12-05 00:58:10 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,20 @@
+$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
-  puts status
+  puts $status
 end
+
+def link 
+ phrases = "It's Dangerous To Go Alone! Take This."
+ return phrases
+end 
+
+def any_phrase(phrase = 'Do A Barrel Roll!')
+  phrase = "Do A Barrel Roll!"
+end 
